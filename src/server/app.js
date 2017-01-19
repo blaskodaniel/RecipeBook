031c6322a1e0ebbe1457195f7a654949d4c82470
Dániel Blaskó <blaskod@sensenet.com>
2017-01-19 10:44:38 +0100

    test change

--- src/server/app.js
+++ src/server/app.js
@@ -4,7 +4,7 @@ var morgan = require('morgan'); // logger
 var bodyParser = require('body-parser');
 
 var app = express();
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 3100));
 
 app.use('/', express.static(__dirname + '/../../dist'));
 
@@ -23,67 +23,67 @@ var Cat = require('./cat.model.js');
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB');
 
-  // APIs
-  // select all
-  app.get('/cats', function(req, res) {
-    Cat.find({}, function(err, docs) {
-      if(err) return console.error(err);
-      res.json(docs);
+    // APIs
+    // select all
+    app.get('/cats', function(req, res) {
+        Cat.find({}, function(err, docs) {
+            if (err) return console.error(err);
+            res.json(docs);
+        });
     });
-  });
 
-  // count all
-  app.get('/cats/count', function(req, res) {
-    Cat.count(function(err, count) {
-      if(err) return console.error(err);
-      res.json(count);
+    // count all
+    app.get('/cats/count', function(req, res) {
+        Cat.count(function(err, count) {
+            if (err) return console.error(err);
+            res.json(count);
+        });
     });
-  });
 
-  // create
-  app.post('/cat', function(req, res) {
-    var obj = new Cat(req.body);
-    obj.save(function(err, obj) {
-      if(err) return console.error(err);
-      res.status(200).json(obj);
+    // create
+    app.post('/cat', function(req, res) {
+        var obj = new Cat(req.body);
+        obj.save(function(err, obj) {
+            if (err) return console.error(err);
+            res.status(200).json(obj);
+        });
     });
-  });
 
-  // find by id
-  app.get('/cat/:id', function(req, res) {
-    Cat.findOne({_id: req.params.id}, function(err, obj) {
-      if(err) return console.error(err);
-      res.json(obj);
-    })
-  });
-
-  // update by id
-  app.put('/cat/:id', function(req, res) {
-    Cat.findOneAndUpdate({_id: req.params.id}, req.body, function(err) {
-      if(err) return console.error(err);
-      res.sendStatus(200);
-    })
-  });
-
-  // delete by id
-  app.delete('/cat/:id', function(req, res) {
-    Cat.findOneAndRemove({_id: req.params.id}, function(err) {
-      if(err) return console.error(err);
-      res.sendStatus(200);
+    // find by id
+    app.get('/cat/:id', function(req, res) {
+        Cat.findOne({ _id: req.params.id }, function(err, obj) {
+            if (err) return console.error(err);
+            res.json(obj);
+        })
     });
-  });
+
+    // update by id
+    app.put('/cat/:id', function(req, res) {
+        Cat.findOneAndUpdate({ _id: req.params.id }, req.body, function(err) {
+            if (err) return console.error(err);
+            res.sendStatus(200);
+        })
+    });
+
+    // delete by id
+    app.delete('/cat/:id', function(req, res) {
+        Cat.findOneAndRemove({ _id: req.params.id }, function(err) {
+            if (err) return console.error(err);
+            res.sendStatus(200);
+        });
+    });
 
 
-  // all other routes are handled by Angular
-  app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname,'/../../dist/index.html'));
-  });
+    // all other routes are handled by Angular
+    app.get('/*', function(req, res) {
+        res.sendFile(path.join(__dirname, '/../../dist/index.html'));
+    });
 
-  app.listen(app.get('port'), function() {
-    console.log('Angular 2 Full Stack listening on port '+app.get('port'));
-  });
+    app.listen(app.get('port'), function() {
+        console.log('Angular 2 Full Stack listening on port ' + app.get('port'));
+    });
 });
 
 module.exports = app;
