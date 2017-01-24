af19d009c104f06b30177573643677fd192b7788
Dániel Blaskó <blaskod@sensenet.com>
2017-01-24 16:37:44 +0100

    Create recipe patch

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hozzavalo } from './../../models/hozzavalo.model';
 
 @Component({
@@ -7,20 +7,34 @@ import { Hozzavalo } from './../../models/hozzavalo.model';
     styleUrls: ['./create.component.css']
 })
 
-export class CreateComponent{
+export class CreateComponent implements OnInit{
     private food: Array<String>;
     private counter: Number;
     private hozzavalo: Hozzavalo;
-    recipename: String;
+    recipename: String;ujhozzavalo:String;
+    private hozzavalok: Array<Hozzavalo>;
 
     constructor(){
         this.counter = 1;
         this.hozzavalo = new Hozzavalo();
+        this.hozzavalo.name = "";
+        this.hozzavalok = [];
     }
 
-    newItem($event){
-        console.log("Új hozzávaló:");
-        this.hozzavalo.name = "1 kg burgonya";
+    newItem(item){
+        console.log("Új hozzávaló: "+item);
+        let newitem = new Hozzavalo;
+        newitem.name = item;
+        this.hozzavalok.push(newitem);
+        this.ujhozzavalo = "";
     }
 
+    removeItem(item){
+        console.log("Hozzávaló törlése: "+item);
+        this.hozzavalok.splice(this.hozzavalok.indexOf(item),1);
+    }
+
+    ngOnInit() {
+        console.log(this.hozzavalok.length);
+    }
 }
