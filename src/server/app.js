38a616f8b31f1257529a3681cfdd56157ae4209b
Daniel Blasko <blaskoi@gmail.com>
2017-02-27 19:34:27 +0100

    fix

--- src/server/app.js
+++ src/server/app.js
@@ -22,7 +22,7 @@ app.use(morgan('dev'));
 app.use(function(req, res, next) {
     //allow cross origin requests
     res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", true);
     next();
